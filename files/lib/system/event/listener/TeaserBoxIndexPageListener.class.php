6fe4e0927af4ac021cf3b572db7b836aed506d82
Matthias Kittsteiner <mk@kittblog.com>
2012-08-10 11:57:23 +0200

    added compact design

--- files/lib/system/event/listener/TeaserBoxIndexPageListener.class.php
+++ files/lib/system/event/listener/TeaserBoxIndexPageListener.class.php
@@ -5,9 +5,9 @@ require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
 /**
  * Displays the teaser box
  *
- * @package	com.alexanderzwei.wbb.teaserbox
- * @author	Alexander 'alexanderzwei' Pankow
- * @copyright	2009 Alexander Pankow
+ * @package	de.kittblog.wcf.teaserbox
+ * @author	Alexander 'alexanderzwei' Pankow; Matthias Kittsteiner
+ * @copyright	2009 Alexander Pankow; 2011-2012 Matthias Kittsteiner
  * @license	LGPL <http://www.gnu.org/licenses/lgpl.html>
  * @subpackage	system.event.listener
  * @category	Community Framework
@@ -75,33 +75,92 @@ class TeaserBoxIndexPageListener implements EventListener {
 		else {
 			$activeBgColor = TEASERBOX_ACTIVENAV_BGCOLOR;
 		}
+		if (!TEASERBOX_NAV_BORDERINNER) {
+			$borderInner = 'transparent';
+		}
+		else {
+			$borderInner = TEASERBOX_NAV_BORDERINNER;
+		}
+		if (!TEASERBOX_NAV_BORDEROUTER) {
+			$borderOuter = 'transparent';
+		}
+		else {
+			$borderOuter = TEASERBOX_NAV_BORDEROUTER;
+		}
 
-		WCF::getTPL()->append('specialStyles',
-			'<link type="text/css" rel="stylesheet" href="'.RELATIVE_WCF_DIR.'style/teaserBox.css" />'.
-			'<style type="text/css">
-			<!--
-			.teaserBoxContent, ul.teaserBoxTeaser li a {
-				height: ' . TEASERBOX_HEIGHT . 'px;
-			}
-			ul.teaserBoxNavigation li a span {
-				color: ' . TEASERBOX_NAV_FONTCOLOR . ';
-				' . $fontShadowColor . ';
-			}
-			ul.teaserBoxNavigation li.activeTeaser a span {
-				color: ' . TEASERBOX_ACTIVENAV_FONTCOLOR . ';
-				' . $activeFontShadowColor . ';
-			}
-			ul.teaserBoxNavigation li.teaserNav {
-				background-color: ' . $bgColor . ';
-			}
-			ul.teaserBoxNavigation li.activeTeaser {
-				background-color: ' . $activeBgColor . ';
-			}
-			-->
-			</style>'
-		);
+		if (TEASERBOX_COMPACT == 1) {
+			// calculate navigation height
+			$navigationHeight = TEASERBOX_HEIGHT - 30;
+
+			WCF::getTPL()->append('specialStyles',
+				'<link type="text/css" rel="stylesheet" href="'.RELATIVE_WCF_DIR.'style/extra/teaserBox-compact.css" />'.
+				'<style type="text/css">
+				<!--
+				ul.teaserBoxNavigation {
+					top: ' . $navigationHeight . 'px;
+				}
+
+				.teaserBoxContent, ul.teaserBoxTeaser li a {
+					height: ' . TEASERBOX_HEIGHT . 'px;
+				}
+
+				ul.teaserBoxNavigation li a span {
+					color: ' . TEASERBOX_NAV_FONTCOLOR . ';
+					' . $fontShadowColor . ';
+				}
+
+				ul.teaserBoxNavigation li.activeTeaser a span {
+					color: ' . TEASERBOX_ACTIVENAV_FONTCOLOR . ';
+					' . $activeFontShadowColor . ';
+				}
+
+				ul.teaserBoxNavigation li.teaserNav {
+					background-color: ' . $bgColor . ';
+					border: 1px solid ' . $borderOuter . ';
+				}
+
+				ul.teaserBoxNavigation li.activeTeaser {
+					background-color: ' . $activeBgColor . ';
+				}
+
+				ul.teaserBoxNavigation li a {
+					border: 1px solid ' . $borderInner . ';
+				}
+				-->
+				</style>'
+			);
+		}
+		else {
+			WCF::getTPL()->append('specialStyles',
+				'<link type="text/css" rel="stylesheet" href="'.RELATIVE_WCF_DIR.'style/extra/teaserBox.css" />'.
+					'<style type="text/css">
+				<!--
+				.teaserBoxContent, ul.teaserBoxTeaser li a {
+					height: ' . TEASERBOX_HEIGHT . 'px;
+				}
+
+				ul.teaserBoxNavigation li a span {
+					color: ' . TEASERBOX_NAV_FONTCOLOR . ';
+					' . $fontShadowColor . ';
+				}
+
+				ul.teaserBoxNavigation li.activeTeaser a span {
+					color: ' . TEASERBOX_ACTIVENAV_FONTCOLOR . ';
+					' . $activeFontShadowColor . ';
+				}
+
+				ul.teaserBoxNavigation li.teaserNav {
+					background-color: ' . $bgColor . ';
+				}
+
+				ul.teaserBoxNavigation li.activeTeaser {
+					background-color: ' . $activeBgColor . ';
+				}
+				-->
+				</style>'
+			);
+		}
 		WCF::getTPL()->append('additionalTopContents', WCF::getTPL()->fetch('teaserBox'));
 	}
-
 }
 ?>
